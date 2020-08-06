786af5408ed2793eb92dc8518f2d27f221e8d6bd
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-08-06 17:40:17 +0200

    all functions

--- script.js
+++ script.js
@@ -31,89 +31,43 @@ sum = 0
   }
   return (sum / arr.length).toFixed(2);
 }
-console.log(getAverageMark(students[0]));
+console.log(getAverageMark(students[2]));
 
 // #3 getStudentInfo
 function getStudentInfo (students) {
-  arr = 
+  arrInfo = {}
+  arrInfo.name = students.name
+  arrInfo.course = students.course
+  arrInfo['averageMark'] = getAverageMark(students)
+  return arrInfo
 }
+console.log(getStudentInfo(students[1]));
 
-// const getSubjects = students => Object.keys(students.subjects).map(element => element[0].toUpperCase() + element.slice(1));
-// console.log((getSubjects(students[0])).replace('_', ' '));
+// #4 getStudentsNames
+getStudentsNames = students => {return students.map(element => element.name).sort()}
+console.log(getStudentsNames(students));
 
-// function getSubjects (student) {
-//   keys = Object.keys(students[0].subjects)
+// #5 getBestStudent
+function getBestStudent (students) {
+  bestMark = 0
+  for (let i = 0; i < students.length; i++) {
+    if (bestMark < getAverageMark(students[i])) {
+      bestMark = getAverageMark(students[i])
+      bestStudent = students[i].name
+    }
+  }
+    return bestStudent
+}
+console.log(getBestStudent(students));
 
-
-// function getSubjects(students) {
-//   keys = Object.keys(students.subjects)
-//   modifiedArr = []
-//   keys.forEach((element, index) => {
-//     modifiedArr[index] = element.charAt(0).toUpperCase() + element.slice(1)
-//     modifiedArr[index] = modifiedArr[index].replace('_', ' ')
-//   })
-//     return modifiedArr;
-// }
-// console.log(getSubjects(students[0]));
-// }
-///////////////////////////////////////////
-// let total = 0;
-// function recurs(number) {
-//   if (number > 0) {
-//     recurs(number - 1);
-//   }
-//   total += number;
-//   console.log(total);
-//   console.log(number);
-// }
-// recurs(2);
-//
-// // console.log(total);
-// // for (let n = 0; n < students.length; n++)
-// // keys = [Object.keys(students[n].subjects)]
-// // for (let i = 0; i < keys.length; i++)
-// // {console.log(keys[i]);}
-// // for (let n = 0; n < students.length; n++)
-// // let keys = Object.keys(students[1].subjects);
-// // console.log(keys);
-// const arr1 = [1, 2, 3, 4, 5];
-// let [per, vto, tre, ...ost] = [0, ...arr1];
-// console.log(per);
-// console.log(vto);
-// console.log(tre);
-// console.log(ost);
-//
-// const obj5 = {
-//   a: 'a',
-//   inner: {
-//     innerA: 'inner2',
-//   },
-//   b: 'b',
-// }
-// for (let key in obj5) {
-//   // console.log(obj5[key]);
-//   console.log(key);
-// }
-//
-//
-//
-// obj1 = {
-//   a: 'f',
-//   inner: {
-//     innerA: 'inner1',
-//   }
-// }
-//
-// obj4 = {name: 'Ivan', course: 2}
-// // let obj2 = {};
-// obj2 = JSON.parse(JSON.stringify(obj1))
-// console.log(obj1);
-// console.log(obj2);
-// console.log(obj1 === obj2);
-// // obj2 = obj1;
-//
-// obj2.a = 'z',
-// console.log(obj1);
-// console.log(obj2);
-// const obj3 = Object.assign({}, obj1, obj2.a)
-// console.log(typeof obj3);
+// #6 calculateWordLetters
+function calculateWordLetters (str) {
+  obj = {}
+  for (i = 0; i < str.length; i++) {
+    chart = str[i]
+    count = obj[chart]
+    obj[chart] = count ? count +1 : 1
+  }
+  return obj;
+}
+console.log(calculateWordLetters('text'));
